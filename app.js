df3f9d3ff93448e6e2a3e61f7dc5f0d37105a066
T. Sreenivasulu <59993636+Sreenivas7463@users.noreply.github.com>
2026-05-30 10:41:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,4 +26,6 @@ app.use('/slug', newsRouter)
 
 
 // Listen on port 5000
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
+});
